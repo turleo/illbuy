34a9fb6c8fdcae2586f89184129888394ab5d11b
Gitea <gitea@fake.local>
2020-07-08 22:30:25 +1000

    Login required for dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def all_lists(request):
     return render(request, 'all.html')
 
 
+@login_required
 def show_list(request, id):
     pass
