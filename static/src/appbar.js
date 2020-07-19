75ad4857a8fc331f84da79f91b1b1b45650bc15b
turleo <leonid.tureyskiy@ya.ru>
2020-07-19 15:18:33 +1000

    menu can be added

--- static/src/appbar.js
+++ static/src/appbar.js
@@ -3,23 +3,43 @@ import React, { Component } from "react";
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
-
-function TopAppBar(){
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+const TopAppBar = (props) => {
+    const classes = useStyles();
     return (
         <React.Fragment>
-            <AppBar position="fixed">
-                <Toolbar>
-                    {document.location.pathname != '/dashboard/' && <IconButton color="inherit" aria-label="back" onClick={() => window.history.back()}>
-                        <ArrowBackIcon />
-                    </IconButton>}
-                    <Typography>{document.getElementsByTagName('title')[0].innerHTML}</Typography>
-                </Toolbar>
-            </AppBar>
-            <br/><br/><br/><br/>
+            <div className={classes.root}>
+                <AppBar position="fixed">
+                    <Toolbar>
+                        {document.location.pathname != '/dashboard/' &&
+                        <IconButton color="inherit" aria-label="back" onClick={() => window.history.back()}
+                                    className={classes.menuButton}>
+                            <ArrowBackIcon/>
+                        </IconButton>}
+                        <Typography
+                            className={classes.title}>{document.getElementsByTagName('title')[0].innerHTML}
+                        </Typography>
+                        {props.children}
+                    </Toolbar>
+                </AppBar>
+                <br/><br/><br/><br/>
+            </div>
         </React.Fragment>
     )
 }
