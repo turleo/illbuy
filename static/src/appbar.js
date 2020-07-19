7f607ed4d259c03f7747ec3df672be5d621d03ec
turleo <leonid.tureyskiy@ya.ru>
2020-07-19 15:18:54 +1000

    menu can be added

--- static/src/appbar.js
+++ static/src/appbar.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
