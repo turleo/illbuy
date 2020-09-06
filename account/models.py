22b5913fcf5d390d891872e56097ba066c07b6b7
turleo <leonid.tureyskiy@ya.ru>
2020-09-06 16:03:07 +1000

    now old login method is working  with websocket

--- account/models.py
+++ account/models.py
@@ -1,9 +1,1 @@
-import uuid
 
-from django.contrib.auth.models import User
-from django.db import models
-
-
-class Token(models.Model):
-    user = models.ForeignKey(User, models.CASCADE)
-    token = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False, unique=True)
--- websocket/consumers.py
+++ websocket/consumers.py
@@ -1,54 +1,25 @@
 import json
-import uuid
 
 from channels.generic.websocket import WebsocketConsumer
-from django.contrib.auth import authenticate
 
-from account.models import Token
 from lists.models import List
 
 
 class APIConsumer(WebsocketConsumer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.user = None
-
     def connect(self):
         self.accept()
+        if self.scope['user'].is_anonymous:
+            self.send('403')
+            self.disconnect(403)
+        else:
+            out = []  # output json
+            lists = List.objects.filter(owner=self.scope['user'])
+            for i in lists:
+                out.append({"id": i.id, "name": i.name})
+            self.send(text_data=json.dumps(out))
 
     def disconnect(self, close_code):
         pass
 
     def receive(self, text_data):
-        if self.user is None:
-            try:
-                username, password = text_data.split(':')
-                user = authenticate(username=username, password=password)
-                if user is None:
-                    self.send(text_data='403')
-                else:
-                    self.user = user
-                    token = Token(user=self.user)
-                    token.save()
-                    self.send(text_data=json.dumps({'token': str(token.token)}))
-                    out = []  # output json
-                    lists = List.objects.filter(owner=self.user)
-                    for i in lists:
-                        out.append({"id": i.id, "name": i.name})
-                    self.send(text_data=json.dumps(out))
-            except ValueError:
-                try:
-                    token = uuid.UUID(text_data)
-                    try:
-                        token_model = Token.objects.get(token=token)
-                        self.user = token_model.user
-                        out = []  # output json
-                        lists = List.objects.filter(owner=self.user)
-                        for i in lists:
-                            out.append({"id": i.id, "name": i.name})
-                        self.send(text_data=json.dumps(out))
-                    except Token.DoesNotExist:
-                        self.send('403')
-
-                except ValueError:
-                    self.send(text_data='403')
+        pass
